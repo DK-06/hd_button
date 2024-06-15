b9d469bfc86be34e31a768791f8ca1839c0b94c3
DK06 <codecracker86@gmail.com>
2024-06-15 21:14:59 +0530

    version changes

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<hardware_buttons/HardwareButtonsPlugin.h>)
+#import <hardware_buttons/HardwareButtonsPlugin.h>
+#else
+@import hardware_buttons;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [HardwareButtonsPlugin registerWithRegistrar:[registry registrarForPlugin:@"HardwareButtonsPlugin"]];
+}
+
+@end
